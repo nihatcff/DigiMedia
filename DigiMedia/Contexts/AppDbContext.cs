314cacb2fe9872a1e60d0b4f96351ef89df8bbab
nihatcff <nihatrkh-mpa101@code.edu.az>
2026-01-19 09:40:53 +0400

    Added Configurations

---
+++ DigiMedia/Contexts/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using DigiMedia.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DigiMedia.Contexts
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Category> Categories  { get; set; }
+
+    }
+}
